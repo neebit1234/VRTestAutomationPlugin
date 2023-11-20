25924741e4831930e2963dcfcdb1eb33aed8cdc2
Joel Hunt <joelmartinhunt@gmail.com>
2023-11-20 16:59:21 +0000

    Generating the plugin to work on

---
+++ Plugins/VRTestAutomationPlugin/Source/VRTestAutomationPlugin/Private/VRTestAutomationPluginBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "VRTestAutomationPluginBPLibrary.h"
+#include "VRTestAutomationPlugin.h"
+
+UVRTestAutomationPluginBPLibrary::UVRTestAutomationPluginBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UVRTestAutomationPluginBPLibrary::VRTestAutomationPluginSampleFunction(float Param)
+{
+	return -1;
+}
+
